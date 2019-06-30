3737667f375d7e9889572233d1eb8699c2bd5e72
Carsonljc <30126272+Carsonljc@users.noreply.github.com>
2019-06-30 13:09:12 -0700

    chrome listener
    close all chrome tabs and start chrome using the command in chrome_extractor.py.

---
+++ chrome_extractor.py
@@ -0,0 +1,24 @@
+import pychrome
+
+def Listener():
+    
+    # start chrome --remote-debugging-port=9222
+    def __init__(self, url="http://127.0.0.1:9222"):
+        self.browser = pychrome.Browser(url=url)
+        self.tab = browser.new_tab()
+        self.tab.set_listener("Network.requestWillBeSent", self.request_will_be_sent)
+        self.tab.set_listener("Network.webSocketFrameReceived", self.web_socket_created)
+
+    def start(self):
+        tab.start()
+        tab.call_method("Network.enable")
+        tab.Page.navigate(url="https://www.nitrotype.com/race", _timeout=5)
+
+    @staticmethod
+    def request_will_be_sent(**kwargs):
+        print("loading: %s" % kwargs.get('request').get('url'))
+
+    @staticmethod
+    def web_socket_created(**kwargs):
+        print("entered")
+        print(kwargs.get('response').get('payloadData'))
--- typer.py
+++ typer.py
@@ -2,9 +2,10 @@ import time
 import numpy as np
 import decode
 import re
+import chrome_extractor
 from pyautogui import press
 
-DEBUG = False
+DEBUG = True
 
 class Typer:
     def __init__(self, wpm):
@@ -23,24 +24,21 @@ class Typer:
         print(self.stream, " : ", self.msg, " : ", self.status, " : ", self.line)
 
 
-
 #4{"stream":"race","msg":"status","payload":{"nitros":3,"status":"countdown","l":"D2 94FD [D6EFAD:5 C@32= @E 6?F>>: E@? D: ==:ED {u} 69E [D6E2E$ 56E:?& 69E ?: D6F826= DEC@AD =2?@:DD67@CA C@;2> C69E@ 69E D2 J=E?6FB6C7 D2 E@? 98F@9E=p"}}
 def master():
     type_bot = Typer(30)
     decoder = decode.Decoder(obfuscated="2", original="a")
 
-
-    if DEBUG:
-        message = """#4{"stream":"race","msg":"status","payload":{"nitros":3,"status":"countdown","l":"D2 94FD [D6EFAD:5 C@32= @E 6?F>>: E@? D: ==:ED {u} 69E [D6E2E$ 56E:?& 69E ?: D6F826= DEC@AD =2?@:DD67@CA C@;2> C69E@ 69E D2 J=E?6FB6C7 D2 E@? 98F@9E=p"}}"""
-    else:
-        message = input ("Enter message: ")
+    message = input ("Enter message: ")
     type_bot.extract_text(message, decoder)
     
     time.sleep(3)
     for character in type_bot.line:
         press(character)
-        time.sleep(0.3)
-
+        amount_to_wait = np.random.normal(loc=0.171,scale=0.01)
+        if DEBUG:
+            print(amount_to_wait)
+        time.sleep(amount_to_wait) 
 
 if __name__ == '__main__':
     master()
